8ce02ea12ae23daf59aa666a3396d719ca19a172
Brad LaMotte <bradlamotte@gmail.com>
2017-10-03 15:53:04 -0400

    Wrap app test with redux provider

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,8 +1,14 @@
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import reducer from '../reducers/index';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>, div);
 });
